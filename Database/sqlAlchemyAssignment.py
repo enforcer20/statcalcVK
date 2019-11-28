fce17050a55db2447174c06cf6e49da4666bc290
Karan <kwalanj@gmail.com>
2019-11-27 19:00:18 -0500

    Feature: Created Order and Orderline table and created relationships

--- Database/sqlAlchemyAssignment.py
+++ Database/sqlAlchemyAssignment.py
@@ -1,56 +1,57 @@
-from sqlalchemy import Column, Integer, String, Numeric, ForeignKey
-from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, Numeric, ForeignKey, Float
 from sqlalchemy.orm import sessionmaker, Session, relationship
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('sqlite:////web/Sqlite-Data/example.db')
 
-Session = sessionmaker(bind=engine)
-session = Session()
-
 Base = declarative_base()
 
 
-class Customer(Base):
-    __tablename__ = 'Customer'
+Base.metadata.drop_all(engine)
 
-    id = Column(Integer, primary_key=True)
-    first_name = Column(String)
-    last_name = Column(String)
-    username = Column(String)
-    email = Column(String)
-    address = Column(String)
-    town = Column(String)
+
+class Customer(Base):
+    __tablename__ = 'customers'
+    id = Column(Integer(), primary_key=True)
+    first_name = Column(String(100), nullable=False)
+    last_name = Column(String(100), nullable=False)
+    username = Column(String(50), nullable=False)
+    email = Column(String(200), nullable=False)
+    address = Column(String(200), nullable=False)
+    town = Column(String(50), nullable=False)
+    orders = relationship("Order", backref='customer')
 
 
 class Item(Base):
-    __tablename__ = 'Item'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    cost_price = Column(Numeric)
-    selling_price = Column(Numeric)
-    quantity = Column(Integer)
+    __tablename__ = 'items'
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(200), nullable=False)
+    cost_price = Column(Numeric(10, 2), nullable=False)
+    selling_price = Column(Numeric(10, 2), nullable=False)
+    quantity = Column(Integer(), nullable=False)
 
 
 class Order(Base):
-    __tablename__ = 'Order'
-
-    customer = Column(String, primary_key=True)
-    OrderLine = relationship("OrderLine")
+    __tablename__ = 'orders'
+    id = Column(Integer(), primary_key=True)
+    customer_id = Column(Integer(), ForeignKey('customers.id'))
 
 
 class OrderLine(Base):
-    __tablename__ = 'OrderLine'
-
-    id = Column(Integer, primary_key=True)
-    order = Column(String, ForeignKey('Order.customer'))
-    item = Column(String)
-    quantity = Column(Numeric)
+    __tablename__ = 'order_lines'
+    id = Column(Integer(), primary_key=True)
+    order_id = Column(Integer(), ForeignKey('orders.id'))
+    item_id = Column(Integer(), ForeignKey('items.id'))
+    quantity = Column(Integer())
+    order = relationship("Order", backref='order_lines')
+    item = relationship("Item")
 
 
 Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
 
 c1 = Customer(first_name='Toby',
               last_name='Miller',
@@ -70,7 +71,7 @@ c2 = Customer(first_name='Scott',
 
 session.add(c1)
 session.add(c2)
-
+session.new
 session.commit()
 
 c3 = Customer(
@@ -109,16 +110,3 @@ c6 = Customer(first_name='Scott',
 
 session.add_all([c3, c4, c5, c6])
 session.commit()
-
-i1 = Item(name='Chair', cost_price=9.21, selling_price=10.81, quantity=5)
-i2 = Item(name='Pen', cost_price=3.45, selling_price=4.51, quantity=3)
-i3 = Item(name='Headphone', cost_price=15.52, selling_price=16.81, quantity=50)
-i4 = Item(name='Travel Bag', cost_price=20.1, selling_price=24.21, quantity=50)
-i5 = Item(name='Keyboard', cost_price=20.1, selling_price=22.11, quantity=50)
-i6 = Item(name='Monitor', cost_price=200.14, selling_price=212.89, quantity=50)
-i7 = Item(name='Watch', cost_price=100.58, selling_price=104.41, quantity=50)
-i8 = Item(name='Water Bottle', cost_price=20.89, selling_price=25, quantity=50)
-
-session.add_all([i1, i2, i3, i4, i5, i6, i7, i8])
-session.commit()
-
